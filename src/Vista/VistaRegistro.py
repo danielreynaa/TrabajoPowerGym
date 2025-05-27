f0e9afd0455b12cdeada599cfc7e15176d2bcd4e
Fernando Pardo <fparda00@estudiantes.unileon.es>
2025-05-27 19:41:09 +0200

    falta registrar los inserts

--- src/Vista/VistaRegistro.py
+++ src/Vista/VistaRegistro.py
@@ -2,7 +2,7 @@
 from PyQt5.QtWidgets import QMainWindow 
 from PyQt5 import uic
 
-Form_Registro, Window_Registro = uic.loadUiType("./src/vista/Ui/VistaRegistro.ui")
+Form_Registro, Window_Registro = uic.loadUiType("./src/Vista/Ui/VistaRegistro.ui")
 
 class VistaRegistro(QMainWindow, Form_Registro):
     def __init__(self):
