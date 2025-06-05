4d2cb777e8721c577782d1ece213c6d2e233621b
David Caminero <dcamic00@estudiantes.unileon.es>
2025-06-05 10:40:36 +0200

    Distintos usuarios

---
+++ src/Vista/VistaMenuAdministrador.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5 import uic
+
+class VistaMenuAdministrador(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("src/Vista/Ui/VistaMenuAdministrador.ui", self)
