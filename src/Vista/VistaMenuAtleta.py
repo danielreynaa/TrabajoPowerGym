72250b3c491ebc0e6c460b4f1541ba5b31708b48
David Caminero <dcamic00@estudiantes.unileon.es>
2025-06-06 18:01:05 +0200

    Arreglando MenuAtleta

--- src/Vista/VistaMenuAtleta.py
+++ src/Vista/VistaMenuAtleta.py
@@ -1,31 +1,9 @@
-from PyQt5.QtWidgets import QApplication
-import sys
-from .VistaLogin import Login
-from src.Vista.VistaLogin import Login
+# src/Vista/VistaMenuAtleta.py
 
-class ControladorAplicacion:
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5 import uic
+
+class VistaMenuAtleta(QMainWindow):
     def __init__(self):
-        self.app = QApplication(sys.argv)
-        self.ventana_login = Login()
-        self.ventana_login.botonaceptar.clicked.connect(self.validar_login)
-        self.ventana_login.show()
-
-    def validar_login(self):
-        # Aquí se valida el login
-        usuario = self.ventana_login.Nombreusuario.text()
-        contrasena = self.ventana_login.Contrasena.text()
-
-        if usuario == "admin" and contrasena == "1234":  # Login simulado
-            print("Login exitoso. Abriendo VistaPrincipal...")
-            self.ventana_login.close()
-            self.ventana_principal = VistaPrincipal()
-            self.ventana_principal.show()
-        else:
-            print("Credenciales incorrectas")
-
-    def ejecutar(self):
-        sys.exit(self.app.exec_())
-
-if __name__ == "__main__":
-    app = ControladorAplicacion()
-    app.ejecutar()
+        super().__init__()
+        uic.loadUi("src/Vista/Ui/VistaMenuAtleta.ui", self)
