5e29cfb3e63d05532a67fa3255e17f7eb578ad81
David Caminero <dcamic00@estudiantes.unileon.es>
2025-06-07 10:23:06 +0200

    Arreglando menus

--- src/Vista/VistaMenuAdministrador.py
+++ src/Vista/VistaMenuAdministrador.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import uic
 
 class VistaMenuAdministrador(QMainWindow):
-    def __init__(self):
+    def __init__(self, usuario):
         super().__init__()
         uic.loadUi("src/Vista/Ui/VistaMenuAdministrador.ui", self)
         self.usuario = usuario
--- src/Vista/VistaMenuEntrenador.py
+++ src/Vista/VistaMenuEntrenador.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import uic
 
 class VistaMenuEntrenador(QMainWindow):
-    def __init__(self):
+    def __init__(self, usuario):
         super().__init__()
         uic.loadUi("src/Vista/Ui/VistaMenuEntrenador.ui", self)
         self.usuario = usuario
